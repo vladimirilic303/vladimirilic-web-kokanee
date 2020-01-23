1a8f6489a67903667dcd4d6d186c2eb547eff2ef
Ben Joldersma <ben@maven.io>
2020-01-23 14:04:55 -0800

    getting close to be able to be a good starting point for candidates

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,10 +1,16 @@
-import { Comment } from '../models/Comment'
+import {Comment, subscribe} from '../models/Comment'
+import {useEffect} from "react";
 
 export interface CommentListProps {
     initialComments: Comment[]
 
 }
 const CommentList: React.FC<CommentListProps> = props => {
+    useEffect(() => {
+        subscribe(comment => {
+            console.log('received comment: ', comment)
+        })
+    }, [])
     return <div>
         [YOU CAN PUT COMMENT LIST COMPONENT HERE, OR ANYWHERE YOU WANT REALLY!]
     </div>
--- models/Comment.ts
+++ models/Comment.ts
@@ -15,8 +15,19 @@ const makeComment = ():Comment => ({
 
 export const makeComments = (count: number) => {
     const comments = []
-    for(var i = 0; i < count; i++) {
+    for(let i = 0; i < count; i++) {
         comments.push(makeComment())
     }
     return comments
+}
+
+export const subscribe = (callback: (comment:Comment) => void) => {
+    console.log('subscribing')
+    const produce = () => {
+        callback(makeComment())
+        const interval = Math.random() * 5000 + 2000
+        console.log('next comment in:', interval)
+        setTimeout(produce, interval)
+    }
+    setTimeout(produce, 3000)
 }
