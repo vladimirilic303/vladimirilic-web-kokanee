ecff27a92d84a4a9ed12d8ec8fb7c01a7ebfd741
Hank Yates <hankyates@gmail.com>
2020-02-16 21:29:00 -0600

    shower thought. leveraging hooks better.

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,16 +1,10 @@
 import { Comment } from '../models/Comment'
-import { useEffect, useState, useContext } from 'react'
-import { CommentContext } from '../observables/comments'
+import { useState } from 'react'
+import { commentsSubscribeEffect } from '../observables/comments'
 
 const CommentList: React.FC = props => {
   const [ comments, setComments ] = useState<Comment[]>([])
-  const { comments$ } = useContext(CommentContext)
-  useEffect(() => {
-    let unsubscribe = comments$.subscribe((comment) => {
-      setComments([...comments, comment])
-    })
-    return unsubscribe.unsubscribe.bind(unsubscribe)
-  })
+  commentsSubscribeEffect((comment) => setComments([...comments, comment]))
   return <div>
     [YOU CAN PUT COMMENT LIST COMPONENT HERE, OR ANYWHERE YOU WANT REALLY!]
     there are {comments.length} comments so far.
--- observables/comments.ts
+++ observables/comments.ts
@@ -10,3 +10,20 @@ export const comments$: Observable<Comment> = interval(1000).pipe(
 )
 
 export const CommentContext = React.createContext({comments$})
+export const comments$FromContext = React.useContext(CommentContext)
+export const commentsSubscribeEffect: (callback: (comment: Comment) => any) => void =
+(callback) =>
+  React.useEffect(() => {
+    let unsubscribe = comments$.subscribe(callback)
+    return unsubscribe.unsubscribe.bind(unsubscribe)
+  })
+
+// Or we could abstract this a step further
+//export const subscribeEffect: <T>(observable: Observable<T>) => (callback: (value: T) => any) => void =
+//(callback) =>
+  //React.useEffect(() => {
+    //let unsubscribe = observable.subscribe(callback)
+    //return unsubscribe.unsubscribe.bind(unsubscribe)
+  //})
+//
+//export const commentsSubscribeEffect(comments$)
